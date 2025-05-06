6bd5a08e1dfa21aa01041bb0c2300dfa67bb304e
amee886 <nsecex@gmail.com>
2025-05-06 16:36:40 +0300

    Update vk_api.py

--- vk_api.py
+++ vk_api.py
@@ -3,82 +3,79 @@ from urllib.parse import urlparse
 from decouple import config
 
 
-API_TOKEN = config("API_TOKEN")
 API_VERSION = '5.131'
-input_url = input("Введите URL: ")
 
 
-def is_shorten_link(input_url):
-    short_domains = ["vk.cc"]
+def is_shorten_link(api_token, input_url):
     parsed_url = urlparse(input_url)
-    if parsed_url.netloc in short_domains:
-        return True
-
-
-def shorten_link(API_TOKEN, input_url):
-    url = f'https://api.vk.com/method/utils.getShortLink'
-    params = {
-        'access_token': API_TOKEN,
-        'url': input_url,
-        'v': API_VERSION
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status()
-    data = response.json()
-    short_url = data['response']['short_url']
-    return short_url
-
-
-try:
-    short_url = shorten_link(API_TOKEN, input_url)
-    print(f"Сокращенная ссылка: {short_url}")
-except requests.exceptions.HTTPError:
-    print("Ошибка  в запросе")
-except ValueError as ve:
-    print(ve)
-except Exception as e:
-    print(f"Произошла ошибка: не удалось распознать JSON")
-
-
-def count_clicks(API_TOKEN, short_url):
-    parsed = urlparse(short_url)
-    key = parsed.path[1:]
+    key = parsed_url.path[1:]
     url = f'https://api.vk.com/method/utils.getLinkStats'
     params = {
-        'access_token': API_TOKEN,
+        'access_token': api_token,
         'key': key,
         'v': API_VERSION
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
-    data = response.json()
-    clicks = data["response"]["stats"]
-    return clicks
+    response_data = response.json()
+    return "response" in response_data and response_data["response"]
 
 
-def main(input_url):
-    if is_shorten_link(input_url):
-        short_url = input_url
-        clicks = count_clicks(API_TOKEN, short_url)
-        print("Является короткой ссылкой.")
+def shorten_link(api_token, input_url):
+    url = f'https://api.vk.com/method/utils.getShortLink'
+    params = {
+        'access_token': api_token,
+        'url': input_url,
+        'v': API_VERSION
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    response_data = response.json()
+    short_url = response_data['response']['short_url']
+    return short_url
+
+
+def count_clicks(api_token, short_url):
+    parsed_url = urlparse(short_url)
+    key = parsed_url.path[1:]
+    url = f'https://api.vk.com/method/utils.getLinkStats'
+    params = {
+        'access_token': api_token,
+        'key': key,
+        'v': API_VERSION
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    response_data = response.json()
+
+    if "response" in response_data and "stats" in response_data["response"] and len(response_data["response"]["stats"]) > 0:
+        clicks = response_data["response"]["stats"][0]["views"]
+        return clicks
     else:
-        short_url = shorten_link(API_TOKEN, input_url)
-        clicks = count_clicks(API_TOKEN, short_url)
-        print("Не является короткой ссылкой")
-    return clicks
+        return 0
 
 
-try:
-    clicks = count_clicks(input_url)
-    print(f"Количество кликов по ссылке: {clicks}")
-except requests.exceptions.HTTPError:
-    print("Ошибка в запросе")
-except ValueError as ve:
-    print(ve)
-except Exception as e:
-    print(f"Произошла ошибка: отстутсвуют клики")
+def main():
+    api_token = config("API_TOKEN")
+    input_url = input("Введите URL: ")
+    try:
+        if is_shorten_link(api_token, input_url):
+            clicks = count_clicks(api_token, input_url)
+            print("Является короткой ссылкой.")
+        else:
+            short_url = shorten_link(api_token, input_url)
+            print(f"Сокращенная ссылка: {short_url}")
+            clicks = count_clicks(api_token, short_url)
+            print("Не является короткой ссылкой")
+        print(f"Количество кликов по ссылке: {clicks}")
+
+    except requests.exceptions.HTTPError:
+        print("Ошибка в запросе")
+    except ValueError as ve:
+        print(ve)
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 
 if __name__ == '__main__':
-    main(input_url)
-
+    main()
