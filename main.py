6cf33bfbc17c6f0c491515409f5818547ea83e61
amee886 <nsecex@gmail.com>
2025-05-08 23:51:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import requests
 from urllib.parse import urlparse
 from decouple import config
-
+import argparse
 
 API_VERSION = '5.131'
 
 
-def is_shorten_link(vk_access_key, input_url):
+def is_shorten_link(access_key_vk, input_url):
     parsed_url = urlparse(input_url)
     key = parsed_url.path[1:]
     url = f'https://api.vk.com/method/utils.getLinkStats'
     params = {
-        'access_token': vk_access_key,
+        'access_token': access_key_vk,
         'key': key,
         'v': API_VERSION
     }
@@ -21,10 +21,10 @@ def is_shorten_link(vk_access_key, input_url):
     return "response" in response_data and response_data["response"]
 
 
-def shorten_link(vk_access_key, input_url):
+def shorten_link(access_key_vk, input_url):
     url = f'https://api.vk.com/method/utils.getShortLink'
     params = {
-        'access_token': vk_access_key,
+        'access_token': access_key_vk,
         'url': input_url,
         'v': API_VERSION
     }
@@ -35,12 +35,12 @@ def shorten_link(vk_access_key, input_url):
     return short_url
 
 
-def count_clicks(vk_access_key, short_url):
+def count_clicks(access_key_vk, short_url):
     parsed_url = urlparse(short_url)
     key = parsed_url.path[1:]
     url = f'https://api.vk.com/method/utils.getLinkStats'
     params = {
-        'access_token': vk_access_key,
+        'access_token': access_key_vk,
         'key': key,
         'v': API_VERSION
     }
@@ -54,22 +54,23 @@ def count_clicks(vk_access_key, short_url):
 
 
 def main():
-    vk_access_key= config("VK_ACCESS_KEY")
-    input_url = input("Введите URL: ")
+    parser = argparse.ArgumentParser(description='Сокращение и статистика по ссылкам VK.')
+    parser.add_argument('url', type=str, help='URL для сокращения или проверки статистики')
+    args = parser.parse_args()
+    
+    access_key_vk = config("ACCESS_KEY_VK")
+    input_url = args.url
+
 
     try:
-        if is_shorten_link(vk_access_key, input_url):
-            clicks = count_clicks(vk_access_key, input_url)
+        if is_shorten_link(access_key_vk, input_url):
+            clicks = count_clicks(access_key_vk, input_url)
             print("Является короткой ссылкой.")
             print(f"Количество кликов по ссылке: {clicks}")
         else:
-            short_url = shorten_link(vk_access_key, input_url)
+            short_url = shorten_link(access_key_vk, input_url)
             print(f"Сокращенная ссылка: {short_url}")
     except requests.exceptions.HTTPError:
         print("Ошибка в запросе")
     except ValueError as ve:
         print(ve)
-
-
-if __name__ == '__main__':
-    main()
